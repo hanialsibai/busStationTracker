751673f8fec07f21056e357f7fa6350a3ede0075
hanialsibai <hanialsibai@gmail.com>
2018-01-29 14:37:22 +0000

    Added repeater function,bg img

--- hell_flask/getBusStopData.py
+++ hell_flask/getBusStopData.py
@@ -1,5 +1,7 @@
 import requests
 import json
+import repeatFunction
+from time import sleep
 
 # buses towards north greenwich
 
@@ -7,15 +9,25 @@ def getTime():
 
     r = requests.get('https://api.tfl.gov.uk/StopPoint/490009889Y/Arrivals?app_id=a72839fb&app_key=5c55da50cc16c2863ad401d7445d9ba1')
     json_result = r.json()
+    json_result = sorted(json_result, key=lambda x: x["timeToStation"])
+
     my_bus=[]
     for x in json_result:
-        if x['timeToStation'] / 60 <= 15:
+        if x['timeToStation'] / 60 <= 16:
                 x = str(x['lineName']) + ' to ' + str(x['destinationName']) + ' in ' + str( x['timeToStation'] / 60)
                 my_bus.append(x)
 
-        with open('outfile.txt', 'w') as f:
-            json.dump(my_bus,f,ensure_ascii=False, indent = 4, sort_keys=True)
 
     return my_bus
 
-#foo[i], foo[j] = foo[j], foo[i]
+def outputText(inputx):
+    with open('outfile.txt', 'w') as f:
+        json.dump(inputx,f,ensure_ascii=False, indent = 4, sort_keys=True)
+        
+def repeat():
+    rt = repeatFunction.RepeatedTimer(5,getTime)
+    try:
+        sleep(100000)
+        getTime()
+    finally:
+        rt.stop()
